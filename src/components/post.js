4e34e97d10acaca685e41ad612e850bc3b22b306
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-08-07 01:27:29 +0200

    Review of the Spread Operator and Rendering Post Associated Topics

--- src/components/post.js
+++ src/components/post.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class Post extends Component {
+
+    renderTopics() {
+        // let topics = this.props.associated_topics.map((topic, index) => {
+        //   return <span className="post-topic" key={index}>{topic}</span>
+          let topics = this.props.films.map((films, index) => {
+            return <span className="post-topic" key={index}>{films}</span>
+      })
+        return topics;
+      }
+    
     render() {
         return (
-            <li>
+            <li className="recent-post">
+                <div className="recent-post__title">
                 {this.props.name}
-            </li>
+                </div>
+                
+                <div className="recent-post__topics">
+                    {this.renderTopics()}
+                </div>
+          </li>
         )
     }
 }
