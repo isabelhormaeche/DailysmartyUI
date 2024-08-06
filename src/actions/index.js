0b8301fc5985febf89a23ef19e57729a3a3c1629
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-08-06 15:07:30 +0200

    Used Axios to Perform GET Request on  Redux Thunk Action Dispatcher

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,28 @@
+import { SET_RECENT_POSTS } from './types';
+
+import axios from 'axios';
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        //perform our request in here.
-        console.log("hello");
+        // DISPATCH--> A "messenger" to send API request to the Redux store, 
+        // which then updates the state with the response data.
+        
+        //Perform our request in here.
+        //console.log("hello");
+
+        //axios.get('https://api.dailysmarty.com/posts') ---> The guide´s api is down. I use Swapi API "https://swapi.dev/api/" instead.
+        axios.get("https://swapi.dev/api/films/")
+        //axios.get("https://swapi.dev/api/planets/") 
+                
+        .then(response => {
+            console.log("response data fetchRecentPosts", response.data.results);
+            dispatch({
+            type: SET_RECENT_POSTS,
+            payload: response.data.results
+            })
+        })
+        
+
+
     }
 }
---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,19 @@
+import {
+    SET_RECENT_POSTS
+} from '../actions/types';
+
+const INIT_STATE = {
+    posts: [],
+    recentPosts: []
+}
+
+export default function(state = INIT_STATE, action) {
+    switch (action.types) {
+        case SET_RECENT_POSTS:
+            
+            return [...state]
+    
+        default:
+            return state;
+    }
+}
