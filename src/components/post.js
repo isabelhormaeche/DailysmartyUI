c6757fcf6127973721b282e7f9c893740383caad
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-08-12 13:20:14 +0200

    Finished JS substrings for pretty post link names

--- src/components/post.js
+++ src/components/post.js
@@ -11,16 +11,52 @@ class Post extends Component {
       }
    }
 
-     renderTopics() {
+  renderTopics() {
          // let topics = this.props.associated_topics.map((topic, index) => {
          //   return <span className="post-topic" key={index}>{topic}</span>
-           let topics = this.props.films.map((film, index) => {
-             return <span className="post-topic" key={index}>{film}</span>
-       })
-         return topics;
-       }
-    
+      let topics = this.props.films.map((film, index) => {
+      return <span className="post-topic" key={index}>{film}</span>
+      })
+      return topics;
+  }
+
+  // ....................Pretty Post Link Names................ 
+
+  // Versión usando........ DailySmarty API:
+
+  //getNameForPostLink(str) {
+
+    // var n = str.lastIndexOf('/');
+    // var link = str.substring(n + 1, str.length);
+
+    // if((n+1) == str.length) {
+    //     link = str.slice(0, n);
+    //     n = link.lastIndexOf('/');
+    //     link = str.substring(n + 1, str.length - 1);
+    // }    
+
+    // if(link.includes('.html')) {
+    //     link = link.substring(0, link.length - 5);
+    // }
+    // if(link.includes('.htm')) {
+    //     link = link.substring(0, link.length - 4);
+    // }
+
+    //return link;
+//}
+
+
+ // Versión usando.......... Star wars API, donde los links terminan en formatostarship/XX/:
+getNameForPostLink(str) {
+
+  var n = str.lastIndexOf('/', str.length - 2); // Encuentra el penúltimo '/', ejemplo: "https://swapi.dev/api/starships/48/"
+  
+  var link = str.substring(str.indexOf("starships"), n + 3); // Obtiene el substring desde "starships" hasta el último '/'
+  // Sumo 3 para incluir el nº y el penúltimo "/" , ejemplo sale:  starships/48
  
+  return link;
+}
+
   renderLinks() {
     // Cambio "post_links" por "starships" de mi  swapi API
       //let links = this.props.post_links.map((post_link, index) => {
@@ -30,7 +66,10 @@ class Post extends Component {
                   <div className="post-link__box"></div>
                   <div className="post-link__link">
                       {/* <a href={post_links.link_url}>Useful Link #{index + 1}</a> */}
-                      <a href={starship}> Useful Starship Link #{index + 1}</a>
+                      {/* <a href={post_link.link_url}>{this.getNameForPostLink(post_link.link_url)}</a> */}
+                      
+                      {/* <a href={starship}> Useful Starship Link #{index + 1}</a> */}
+                      <a href={starship}>{this.getNameForPostLink(starship)}</a>
                   </div>
               </div>
           )
